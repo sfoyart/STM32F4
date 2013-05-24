331c8ebb1e79efddd2bc5f04b4e35b34c0b3d2d7
Jmunsch <local@jamesmunsch.com>
2013-05-24 14:08:22 -0500

    modified:   STM32-templates/stm32f4-discovery/lib/libstm32f4.a 	modified:   STM32-templates/stm32f4-discovery/src/main.c 	modified:   STM32-templates/stm32f4-discovery/src/main.c~ 	modified:   STM32-templates/stm32f4-discovery/turds.bin 	modified:   STM32-templates/stm32f4-discovery/turds.elf 	modified:   STM32-templates/stm32f4-discovery/turds.hex 	deleted:    stm32-templates

--- STM32-templates/stm32f4-discovery/src/main.c
+++ STM32-templates/stm32f4-discovery/src/main.c
@@ -59,7 +59,7 @@ int main(void)
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
   /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Pin = /*GPIO_Pin_12 | GPIO_Pin_13|*/ GPIO_Pin_14/*| GPIO_Pin_15*/;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
@@ -69,13 +69,37 @@ int main(void)
   while (1)
   {
 
-    /* PD15 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_15);
-    
+    /* PD12 to be toggled */
+/*    GPIO_SetBits(GPIOD, GPIO_Pin_12);
+*/
     /* Insert delay */
-    Delay(0x7FFFFF);
+/*    Delay(0x3FFFFF);
+*/
+
+
+    /* PD13 to be toggled */
+/*    GPIO_SetBits(GPIOD, GPIO_Pin_13);
+*/
+    /* Insert delay */
+/*    Delay(0x3FFFFF);
+*/
+
+
+    /* PD14 to be toggled */
+    GPIO_SetBits(GPIOD, GPIO_Pin_14);
+    /* Insert delay */
+    Delay(0x3FFFFF);
     
-    GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+
+
+    /* PD15 to be toggled */
+/*    GPIO_SetBits(GPIOD, GPIO_Pin_15);
+*/    
+
+    /* Insert delay */
+/*    Delay(0x7FFFFF);
+*/    
+    GPIO_ResetBits(GPIOD, /*GPIO_Pin_12|GPIO_Pin_13|*/GPIO_Pin_14/*|GPIO_Pin_15*/);
     
     /* Insert delay */
     Delay(0xFFFFFF);
